dc9811d3d5cf74c93532b62ecd04985dece98064
Chitranshi <apigeoneer@gmail.com>
2022-08-04 14:29:57 +0530

    ui: about

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,25 @@
+import "./about.css"
+
+export default function About() {
+  return (
+    <div className="about">
+        <img src="https://img.freepik.com/free-vector/nature-scene-with-river-hills-forest-mountain-landscape-flat-cartoon-style-illustration_1150-37326.jpg?w=2000" alt="" />
+        <div className="hey">hey&#128075;</div>
+        <div className="content">
+            <p>(25:5) autoprefixer: end value has mixed support, consider using flex-end instead
+            (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead, (25:5) autoprefixer: end value has mixed support, consider using flex-end instead
+            (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead, (25:5) autoprefixer: end value has mixed support, consider using flex-end instead
+            (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead, (25:5) autoprefixer: end value has mixed support, consider using flex-end instead</p>
+            <p>(25:5) autoprefixer: end value has mixed support, consider using flex-end instead
+            (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead, (25:5) autoprefixer: end value has mixed support, consider using flex-end instead
+            (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead, (25:5) autoprefixer: end value has mixed support, consider using flex-end instead
+            (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead, (25:5) autoprefixer: end value has mixed support, consider using flex-end instead</p>
+            <p>(25:5) autoprefixer: end value has mixed support, consider using flex-end instead
+            (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead, (25:5) autoprefixer: end value has mixed support, consider using flex-end instead
+            (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead, (25:5) autoprefixer: end value has mixed support, consider using flex-end instead
+            (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead. (25:5) autoprefixer: end value has mixed support, consider using flex-end instead, (25:5) autoprefixer: end value has mixed support, consider using flex-end instead</p>        
+        </div>
+    
+    </div>
+  )
+}
